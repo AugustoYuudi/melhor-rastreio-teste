af2f60dbabdb10337f12f9dc929b324cf7748559
Augusto Shiraishi <augshira@publicisgroupe.net>
2019-07-18 19:20:54 -0300

    Setup da Store + Reta final responsivo

--- src/store.js
+++ src/store.js
@@ -5,12 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    product: {}
   },
-  mutations: {
 
+  getters: {
+    product: state => state.product
+  },
+
+  mutations: {
+    setProduct(state, product) {
+      state.product = product
+    }
   },
   actions: {
+    async getProduct(context) {
+      const response = await fetch('https://demo0655245.mockable.io/v1/tracking?search=PM371835103BR')
 
+      const json = await response.json()
+
+      console.log(json)
+
+      context.commit('setProduct', json)
+    }
   }
 })
---
+++ src/views/RastreioProduto.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="rastreio-produto">
+        {{product}}
+    </div>
+</template>
+
+<style lang="stylus">
+
+</style>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    name: 'rastreio-produto',
+
+    methods: {
+        ...mapActions([
+            'getProduct'
+        ])
+    },
+
+    computed: {
+      ...mapGetters([
+          'product'
+      ])  
+    },
+
+    mounted() {
+        this.getProduct()
+    }
+}
+</script>
